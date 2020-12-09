4ee185b085268aff6469167f4932b383cfb3c076
mathisenj <SYq5UtBN>
2020-12-09 19:04:30 +0100

    update gifts

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -1,29 +1,45 @@
 import React from "react"
 
 function Gift({ data }) {
-  const { title, imageLink, brand, link, price } = data
+  const { title, imageLink, brand, link, price, isTake, otherLink } = data
+
+  const otherLinkJSX = otherLink && (
+    <a href={otherLink} className="gift-link">
+      {otherLink ? "Lien vers le site" : "Contactez Mathis"}
+    </a>
+  )
 
   return data.id % 2 === 0 ? (
-    <div className="gift bg-dark-blue">
+    <div className={`gift bg-dark-blue ${isTake && "disabled"}`}>
       <div className="gift-content">
         <h3>{title}</h3>
         <div className="gift-brand">{brand}</div>
-        <a href={link} className="gift-link">
-          {link ? "Lien vers le site" : "Contactez Mathis"}
-        </a>
+        {!isTake && (
+          <>
+            <a href={link} className="gift-link">
+              {link ? "Lien vers le site" : "Contactez Mathis"}
+            </a>
+            {otherLinkJSX}
+          </>
+        )}
         <div className="gift-price">{price}</div>
       </div>
       <img src={imageLink} alt={title} />
     </div>
   ) : (
-    <div className="gift bg-grey">
+    <div className={`gift bg-grey ${isTake && "disabled"}`}>
       <img src={imageLink} alt={title} />
       <div className="gift-content">
         <h3>{title}</h3>
         <div className="gift-brand">{brand}</div>
-        <a href={link} className="gift-link">
-          {link ? "Lien vers le site" : "Contactez Mathis"}
-        </a>
+        {!isTake && (
+          <>
+            <a href={link} className="gift-link">
+              {link ? "Lien vers le site" : "Contactez Mathis"}
+            </a>
+            {otherLinkJSX}
+          </>
+        )}
         <div className="gift-price">{price}</div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,19 @@ export default function Home() {
     },
     {
       id: 2,
+      title: "Ecran ordinateur",
+      imageLink:
+        "https://www.cdiscount.com/pdt2/x/e/n/1/700x700/sals24f354fhuxen/rw/samsung-s24f354-ecran-gamer-24-fhd-dalle-pls.jpg",
+      brand: "Cdiscount",
+      link:
+        "https://www.cdiscount.com/informatique/ecran-pc/samsung-s24f354-ecran-gamer-24-fhd-dalle-pls/f-10710-sals24f354fhuxen.html?idOffre=426494325#mpos=0|cd&sw=d015f24ca797cab7087870a0bb4e9d27c00ff8c00b6391b512e10e4372cfef1da6520b9b2917f7010993917d2d7d20027d67098d09e5ecc01d3342a387559ab0ea509e7201a0d6ad786dd32b16922cdc9bb6019ce70ca47ddc26a49f9934a0df87bd32cc1ff7ae307226070116e36cd7",
+      otherLink:
+        "https://www.amazon.fr/Samsung-S24F350F-Ecran-54-61-1920x1080/dp/B01BCF0006/ref=sr_1_6?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=ecran%2Bsamsung&qid=1607536751&sr=8-6&th=1",
+      price: "99,99€",
+      isTake: false,
+    },
+    {
+      id: 3,
       title: "Pantalon Cargo - Kaki - Taille S",
       imageLink:
         "https://static.zara.net/photos///2020/I/0/2/p/0706/361/505/2/w/1140/0706361505_6_1_1.jpg?ts=1599725146870",
@@ -23,10 +36,10 @@ export default function Home() {
       link:
         "https://www.zara.com/fr/fr/pantalon-battle-structur%C3%A9-p00706361.html?v1=64726857&v2=1547026",
       price: "49,95€",
-      isTake: false,
+      isTake: true,
     },
     {
-      id: 3,
+      id: 4,
       title: "Tee shirt - Noir - Taille M",
       imageLink:
         "https://static.zara.net/photos///2020/I/0/2/p/9240/320/800/2/w/1140/9240320800_6_1_1.jpg?ts=1595238898154",
@@ -34,7 +47,7 @@ export default function Home() {
       link:
         "https://www.zara.com/fr/fr/t-shirt-basique-compact-p09240320.html?v1=86662663&v2=1546869",
       price: "12,95€",
-      isTake: false,
+      isTake: true,
     },
   ]
 
